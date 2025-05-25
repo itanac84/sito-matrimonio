8deed86fe480836c3f2c9e2ca1021c241d8d0bfe
itanac84 <a.chidini@gmail.com>
2025-05-25 11:55:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+document.querySelectorAll('.page').forEach(section => {
+  observer.observe(section);
+});
